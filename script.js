dff6ff932a0bdf58979bbb6be3aa7885153f2231
Mtalhaa832 <145839465+Mtalhaa832@users.noreply.github.com>
2024-03-23 12:55:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function signup(){
+  const boo = document.getElementById("bbt").style.backgroundColor =" rgb(34, 34, 103)";
+    boo = document.getElementById ("bbt").style.color = "white";
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   let signup = document.getElementById("sign_up").style.display="block";
+//   if(signup == 'block'){
+//     signup.style.display="none"
+//   }
+//   else{
+//     signup.style.display="block"
+//   }
+// }
+function login(){
+ const bo = document.getElementById("bbtt").style.backgroundColor =" rgb(34, 34, 103)";
+  // const login = document.getElementById("bbtt").style.display="none";
+   bo = document.getElementById ("bbtt").style.color = "white";
+
+   
+  }
+  // if(login.style.display == 'none'){
+  //   login.style.display="block";
+  // }
+  // else{
+  //   login.style.display="none";
+  // }
+//   let login = document.getElementById("login").style.display="none";
+//   if(login == 'none'){
+//     login.style.display="block"
+//   }
+//   else{
+//     login.style.display="none"
+//   }
+// }
+// const intt = Document.getElementById("signup");
+// intt.addEventlistner
